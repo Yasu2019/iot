11fe0a406c510800ba695d420591dac18fdf3adc
Yasu2019 <yasuhiro-suzuki@mitsui-s.com>
2023-03-05 18:59:01 +0900

    Revise login_csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,21 @@ CSV.foreach('db/category.csv') do |row|
   
 end
 
+CSV.foreach('db/record/login.csv') do |row|
+  User.create(:id => row[0], :email => row[1], :password => row[2], :name => row[3], :role => row[4])
+end
 
-User.create!(id: 1, email: "yasuhiro-suzuki@mitsui-s.com", password: "password", name: "鈴木康弘"       , role: :staff)
-User.create!(id: 2, email: "y.suzuki.hk@gmail.com",        password: "password", name: "鈴木康弘・gmail", role: :staff)
-User.create!(id: 3, email: "y_suzuki_hk@yahoo.co.jp",      password: "password", name: "鈴木康弘・yahoo", role: :normal)
-User.create!(id: 4, email: "h-hirano@mitsui-s.com",        password: "password", name: "平野東之"       , role: :normal)
-User.create!(id: 5, email: "m-ishii@mitsui-s.com",         password: "password", name: "石井 三明"      , role: :normal)
-User.create!(id: 6, email: "k-seo@mitsui-s.com",           password: "password", name: "瀬尾健太"       , role: :normal)
-User.create!(id: 7, email: "t-hyogo@mitsui-s.com",         password: "password", name: "兵庫貴帆"       , role: :normal)
-User.create!(id: 8, email: "k-asano@mitsui-s.com",         password: "password", name: "浅野係長"       , role: :normal)
-User.create!(id: 9, email: "y-imamura@mitsui-s.com",       password: "password", name: "今村雄二"       , role: :normal)
-User.create!(id:10, email: "ngocanhnguyen@mitsui-s.com",   password: "password", name: "アイン"         , role: :normal)
-User.create!(id:11, email: "admin@iatf.co.jp",             password: "password", name: "ミツイ精密"     , role: :normal)
+#User.create!(id: 1, email: "yasuhiro-suzuki@mitsui-s.com", password: "password", name: "鈴木康弘"       , role: :staff)
+#User.create!(id: 2, email: "y.suzuki.hk@gmail.com",        password: "password", name: "鈴木康弘・gmail", role: :staff)
+#User.create!(id: 3, email: "y_suzuki_hk@yahoo.co.jp",      password: "password", name: "鈴木康弘・yahoo", role: :normal)
+#User.create!(id: 4, email: "h-hirano@mitsui-s.com",        password: "password", name: "平野東之"       , role: :normal)
+#User.create!(id: 5, email: "m-ishii@mitsui-s.com",         password: "password", name: "石井 三明"      , role: :normal)
+#User.create!(id: 6, email: "k-seo@mitsui-s.com",           password: "password", name: "瀬尾健太"       , role: :normal)
+#User.create!(id: 7, email: "t-hyogo@mitsui-s.com",         password: "password", name: "兵庫貴帆"       , role: :normal)
+#User.create!(id: 8, email: "k-asano@mitsui-s.com",         password: "password", name: "浅野係長"       , role: :normal)
+#User.create!(id: 9, email: "y-imamura@mitsui-s.com",       password: "password", name: "今村雄二"       , role: :normal)
+#User.create!(id:10, email: "ngocanhnguyen@mitsui-s.com",   password: "password", name: "アイン"         , role: :normal)
+#User.create!(id:11, email: "admin@iatf.co.jp",             password: "password", name: "ミツイ精密"     , role: :normal)
 
 
 #rails c でカラムの確認
